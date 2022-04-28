695af451be6a517ff3ddbbbfdd57b64ceb99fd38
Lektor <denislour@gmail.com>
2022-04-28 23:36:44 +0700

    [Admin] - Import bootstrap and setup admin panel.

--- imo-admin/src/app/app.module.ts
+++ imo-admin/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
